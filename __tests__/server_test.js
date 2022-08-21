9cfd27b9a98ac0f90170b3f1c4e48bd353fb997a
Artur Todeschini <artodeschini@gmail.com>
2022-08-20 23:37:27 -0300

    preparando para continuidade do projeto

---
+++ __tests__/server_test.js
@@ -0,0 +1,9 @@
+const supertest = require('supertest');
+const app = require('../src/app');
+const request = supertest(app); // integra jest com o app
+
+it('Deve retornar status 200 ao chamar o raiz da aplicacao', () => {
+    return request.get('/').then(res => {
+        expect(res.statusCode).toEqual(200);
+    });
+});
--- src/app.js
+++ src/app.js
@@ -5,24 +5,4 @@ app.get('/', (req, res) => {
     res.json([]);
 });
 
-app.get('/cor/:pessoa', (req, res) => {
-    let pessoa = req.params.pessoa;
-
-    if (pessoa == undefined) {
-        res.json({cor: 'branco'});
-    } else if (pessoa.toUpperCase() == 'ARTUR') {
-        res.json({cor: 'azul'});
-    } else if (pessoa.toUpperCase() == 'MANU') {
-        res.json({cor: 'rosa'});
-    } else if (pessoa.toUpperCase() == 'CATIA') {
-        res.json({cor: 'vermelho'});
-    } else {
-        res.json({cor: 'blabla blabla'});
-    }
-});
-
-// remove do app para poder integrar o jest com express
-// app.listen(8080, () => {
-//     console.log('start app');
-// });
 module.exports = app;
