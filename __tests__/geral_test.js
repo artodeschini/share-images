1247b4ce06d58c5a515968953786b9224aaa758b
Artur Todeschini <artodeschini@gmail.com>
2022-08-20 22:24:22 -0300

    adicionado teste de /cor/nome

--- __tests__/geral_test.js
+++ __tests__/geral_test.js
@@ -19,3 +19,39 @@ test("Ao chamar a url do google deve retornar status 200 com async ", async () =
         throw new Error('I have failed you, Anakin');
     }
 });
+
+it('Deve retornar status 200 ao chamar o raiz da aplicacao', () => {
+    return request.get('/').then(res => {
+        expect(res.statusCode).toEqual(200);
+    });
+});
+
+describe("Teste das cores", () => {
+    it('Deve retornar azul ao chamar /cor/artur', () => {
+        return request.get('/cor/artur').then(res => {
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.cor).toEqual('azul');
+        });
+    });
+
+    it('Deve retornar azul ao chamar /cor/manu', () => {
+        return request.get('/cor/manu').then(res => {
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.cor).toEqual('rosa');
+        });
+    });
+
+    it('Deve retornar azul ao chamar /cor/catia', () => {
+        return request.get('/cor/catia').then(res => {
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.cor).toEqual('vermelho');
+        });
+    });
+
+    it('Deve retornar azul ao chamar /cor/bla', () => {
+        return request.get('/cor/bla').then(res => {
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.cor).toContain('bla');
+        });
+    });
+});
